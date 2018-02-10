3d38a5c0c49e69add0274158ff81148b994ded4b
romastyi <my.beliberda@gmail.com>
2018-02-11 00:24:25 +0300

    Client code generation refactoring.

--- api/src/main/scala/com/github/romastyi/api/client/dsl/WebServicesComponents.scala
+++ api/src/main/scala/com/github/romastyi/api/client/dsl/WebServicesComponents.scala
@@ -2,20 +2,32 @@ package com.github.romastyi.api.client.dsl
 
 import akka.actor.ActorSystem
 import akka.stream.ActorMaterializer
-import play.api.{Application, Configuration}
+import play.api.Configuration
 import play.api.libs.ws.WSClient
 import play.api.libs.ws.ahc.AhcWSClient
 import play.boilerplate.api.client.dsl._
 import scaldi.Module
 
 class WebServicesComponents extends Module {
-  private lazy val config = inject[Configuration].underlying
-  implicit lazy val system: ActorSystem = inject[Application].actorSystem
-  implicit lazy val materializer: ActorMaterializer = ActorMaterializer()
-  implicit lazy val circuitBreakers: CircuitBreakersPanel = AkkaCircuitBreakersPanel.instance(config.getConfig("circuit-breaker"))
-  bind [WSClient] to AhcWSClient()
-  bind [CircuitBreakersPanel] to circuitBreakers
+  bind [WSClient] to {
+    implicit val system: ActorSystem = inject[ActorSystem]
+    implicit val mat: ActorMaterializer = ActorMaterializer()
+    AhcWSClient()
+  }
+  bind [CircuitBreakersPanel] to {
+    implicit val system: ActorSystem = inject[ActorSystem]
+    val config = inject[Configuration].underlying
+    AkkaCircuitBreakersPanel.instance(config.getConfig("circuit-breaker"))
+  }
   // Service locators
-  bind [ServiceLocator] identifiedBy 'config to ServiceLocator.DefaultImpl(config.getConfig("discovery"))
-  bind [ServiceLocator] identifiedBy 'consul to ConsulServiceLocator.instance(config.getConfig("discovery"))
+  bind [ServiceLocator] identifiedBy 'config to {
+    implicit val circuitBreaker: CircuitBreakersPanel = inject[CircuitBreakersPanel]
+    val config = inject[Configuration].underlying
+    ServiceLocator.DefaultImpl(config.getConfig("discovery"))
+  }
+  bind [ServiceLocator] identifiedBy 'consul to {
+    implicit val circuitBreaker: CircuitBreakersPanel = inject[CircuitBreakersPanel]
+    val config = inject[Configuration].underlying
+    ConsulServiceLocator.instance(config.getConfig("discovery"))
+  }
 }
